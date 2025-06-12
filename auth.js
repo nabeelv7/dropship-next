17f02e6a05c901d952709434bba9423be87b0c9a
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-06-12 11:50:03 +0500

    added sqlite

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { db } from "./db";
+// import { DrizzleAdapter } from "@auth/drizzle-adapter";
+// import { db } from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  // adapter: DrizzleAdapter(db),
   providers: [],
 });
